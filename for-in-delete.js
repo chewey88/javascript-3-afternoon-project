003f5d9b8f01bd318d30b517cd73e69402330402
Christopher Sovine <chewey88@gmail.com>
2020-03-06 16:35:51 -0700

    day5-projectforin

--- for-in-delete.js
+++ for-in-delete.js
@@ -39,8 +39,12 @@
   Inside the function showValues, write a for in loop that concatenates each of the property values and returns the concatenated string.
 */
 
-function showValues( obj ) {
-  //Code Here
+function showValues(obj) {
+  let str = ""
+  for (var key in obj) {
+    str += obj[key]
+  }
+  return str
 }
 
 
@@ -54,7 +58,16 @@ function showValues( obj ) {
 */
 
 //Code Here
+function greaterThan10(obj) {
+  for (let key in obj) {
+    if (obj[key] > 10) {
+      obj[key] = 0
+    }
 
+
+  }
+  return obj
+}
 
 
 ////////// PROBLEM 3 //////////
@@ -66,7 +79,12 @@ function showValues( obj ) {
 */
 
 //Code Here
-
+function double(obj) {
+  for (let key in obj) {
+    obj[key] = obj[key] * 2
+  }
+  return obj
+}
 
 
 ////////// PROBLEM 4 //////////
@@ -80,7 +98,15 @@ function showValues( obj ) {
 */
 
 //Code Here
-
+function secrets(obj) {
+  str = ``
+  for (let key in obj) {
+    if (key[0] === `s` && key[1] === `h`) {
+      str += obj[key]
+    }
+  }
+  return str
+}
 
 
 /* 
@@ -111,7 +137,15 @@ function showValues( obj ) {
 */
 
 //Code Here
+function removePassword(obj) {
+  for (let key in obj) {
+    if(key === `password`){
+      delete obj[key]
 
+    }
+  }
+  return obj
+}
 
 
 ////////// PROBLEM 6 //////////
@@ -130,7 +164,11 @@ var deleteTheBigNumbers = {
 */
 
 //Code Here
-
+for(let key in deleteTheBigNumbers){
+  if(deleteTheBigNumbers[key] > 100){
+    delete deleteTheBigNumbers[key]
+  }
+}
 
 
 ////////// PROBLEM 7 //////////
@@ -143,6 +181,14 @@ var deleteTheBigNumbers = {
 */
 
 //Code Here
+function startsWithK(obj){
+  for(let key in obj){
+    if(key[0] === `k`){
+      delete obj[key]
+    }
+  }
+  return obj
+}
 
 
 
@@ -158,5 +204,12 @@ var deleteTheBigNumbers = {
 */
 
 //Code Here
-
+function hiddenTreasure(obj){
+  for(let key in obj){
+    if(!obj[key].includes(`treasure`)){
+      delete obj[key]
+    }
+  }
+  return obj
+}
 
